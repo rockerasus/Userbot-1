78f414a8bf950a1d5fea415161fb859b8532a86d
Devp73 <52715880+Devp73@users.noreply.github.com>
2019-08-27 20:39:47 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -931,7 +931,7 @@ async def hacking (hacked):
         if await hacked.get_reply_message():
             await hacked.edit(
                 "`Targeted Account Hacked successfully 😎......`\n"
-                "`Pay 6969$ to` @anonyCrew `To Remove This Hack...`\n"
+                "`Pay 6969$ to` @devpatel_73 `To Remove This Hack...`\n"
             )
 			  
 @register(outgoing=True, pattern="^.kill$")
