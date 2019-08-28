46f1ef9f1496f010d8c2642f86d7853492200d58
Devp73 <52715880+Devp73@users.noreply.github.com>
2019-08-28 21:06:53 +0530

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -81,12 +81,12 @@ async def bot_support(wannahelp):
 @register(outgoing=True, pattern="^.creator$")
 async def creator(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("[Spyder #DerpGang](https://t.me/spyderzzz)")
+        await e.edit("[Dev](https://t.me/devpatel_73)")
 
 @register(outgoing=True, pattern="^.readme$")
 async def reedme(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("You might want to have a look at the [README.md](https://github.com/spyderzz/UserBot/blob/master/README.md) file.")
+        await e.edit("You might want to have a look at the [README.md](https://github.com/Devp73/UserBot-1/blob/master/README.md) file.")
 
 #
 # Copyright (c) Gegham Zakaryan | 2019
@@ -108,7 +108,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     if not wannasee.text[0].isalpha() and wannasee.text[0] not in ("/", "#", "@", "!"):
-        await wannasee.edit("Click [here](https://github.com/spyderzz/UserBot) to open this lit af repo.")
+        await wannasee.edit("Click [here](https://github.com/Devp73/UserBot-1) to open this lit af repo.")
 
 CMD_HELP.update({
     'random': '.random <item1> <item2> ... <itemN>\
