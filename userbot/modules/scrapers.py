d25a6f15ec20d8b40774aa8735c3b692fc21a7e0
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-08-29 16:44:27 +0530

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -89,7 +89,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="[devpatel](https://t.me/devpatel_73/) Iz Noob Boi af.",
+         caption="[Spyder](https://t.me/spyderzzz/) iz Gawd + peru af.",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
