0df30977078a794ee0e06d15c21a35887495f45c
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-08-29 22:44:32 +0530

    Update extra.py

--- userbot/modules/extra.py
+++ userbot/modules/extra.py
@@ -68,12 +68,12 @@ async def _(event):
 @register(outgoing=True, pattern="^.source$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("https://github.com/spyderzzz/Userbot/")
+        await e.edit("https://github.com/spyderzz/Userbot/")
 
 @register(outgoing=True, pattern="^.readme$")
 async def reedme(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("https://github.com/spyderzzz/Userbot/blob/master/README.md")
+        await e.edit("https://github.com/spyderzz/Userbot/blob/master/README.md")
 
 
 @register(outgoing=True, pattern="^.clock$")
