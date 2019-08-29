fdbbec57c2249d049793ab6d56247d09d067abe4
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-08-29 17:03:57 +0530

    Update invite.py

--- userbot/modules/invite.py
+++ userbot/modules/invite.py
@@ -32,4 +32,4 @@ async def _(event):
                     ))
                 except Exception as e:
                     await event.reply(str(e))
-            await event.edit("Invited Successfully")
+            await event.edit("Nibba Invited Successfully.")
